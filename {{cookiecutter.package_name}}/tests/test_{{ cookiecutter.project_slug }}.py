2f8cf2db31e9189ca3af607d8a4ac3ea8e27923e
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2025-03-19 23:09:29 +0300

    fix: simplify test

--- {{cookiecutter.package_name}}/tests/test_{{ cookiecutter.project_slug }}.py
+++ {{cookiecutter.package_name}}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -1,8 +1,6 @@
+import {{ cookiecutter.project_slug }}
+
+
 def test_import() -> None:
     """Test that the package can be imported without errors."""
-    try:
-        import {{ cookiecutter.project_slug }}
-
-        print({{ cookiecutter.project_slug }}.__version__)
-    except ImportError as e:
-        raise AssertionError("Failed to import the package.") from e
+    assert isinstance({{ cookiecutter.project_slug }}.__name__, str)
