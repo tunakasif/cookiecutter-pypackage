bc649aaa5dee9f3ab49b9a2fc1bb6693656f6360
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2025-03-19 19:25:00 +0300

    refactor: format test file

--- {{cookiecutter.package_name}}/tests/test_{{ cookiecutter.project_slug }}.py
+++ {{cookiecutter.package_name}}/tests/test_{{ cookiecutter.project_slug }}.py
@@ -2,6 +2,7 @@ def test_import() -> None:
     """Test that the package can be imported without errors."""
     try:
         import {{ cookiecutter.project_slug }}
+
         print({{ cookiecutter.project_slug }}.__version__)
     except ImportError as e:
         raise AssertionError("Failed to import the package.") from e
